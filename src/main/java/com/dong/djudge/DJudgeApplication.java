f2e0fc8124994049b6b01f0b15cd6204ac58ceef
pllysun <1125887000@qq.com>
2023-11-22 20:25:15 +0800

    :tada:初始化项目并且实现OI模式的代码编写。

--- src/main/java/com/dong/djudge/DJudgeApplication.java
+++ src/main/java/com/dong/djudge/DJudgeApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ADong
+ */
 @SpringBootApplication
 @MapperScan("com.dong.djudge.mapper")
 public class DJudgeApplication {
